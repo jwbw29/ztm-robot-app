957df459c746e55c083a4dfe3a3c971612099666
Justin Byrd <justinbyrd7@gmail.com>
2023-08-03 15:49:02 -0500

    wip

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+
+const App = () => {
+  return (
+    <>
+      <h1>RoboFriends</h1>
+      <CardList robots={robots} />
+    </>
+  );
+};
+
+export default App;
