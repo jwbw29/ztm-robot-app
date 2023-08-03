536ad7845593ac9374e10c6086672d6bc48e60db
Justin Byrd <justinbyrd7@gmail.com>
2023-08-03 14:16:19 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Hello from "./Hello";
+import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Hello greeting={"Hello" + "React Ninja"} />{" "}
-  </React.StrictMode>
-);
+root.render(<Card />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
